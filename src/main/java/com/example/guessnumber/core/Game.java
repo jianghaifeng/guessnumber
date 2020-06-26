f67d730b25407e8a3bc7abcf404d05733df3d8fc
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-26 11:58:13 +0800

    [refactor] Pass test cases.

--- src/main/java/com/example/guessnumber/core/Game.java
+++ src/main/java/com/example/guessnumber/core/Game.java
@@ -3,7 +3,8 @@ package com.example.guessnumber.core;
 public class Game {
   private String targetNumberStr;
 
-  public Game() {
+  public Game(RandomNumberGenerator generator) {
+    this.targetNumberStr = generator.generate();
   }
 
   public String getTargetNumberStr() {
