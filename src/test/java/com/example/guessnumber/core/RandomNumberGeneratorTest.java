7e144f18163e90087bd2256377df934e5e9b2673
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-26 10:42:05 +0800

    [testcase] should generate number with distinct digits.

--- src/test/java/com/example/guessnumber/core/RandomNumberGeneratorTest.java
+++ src/test/java/com/example/guessnumber/core/RandomNumberGeneratorTest.java
@@ -3,25 +3,37 @@ package com.example.guessnumber.core;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RandomNumberGeneratorTest {
   RandomNumberGenerator generator = new RandomNumberGenerator();
 
   @Test
-  @RepeatedTest(10)
   public void shouldGenerateNumberWith4Charactors() {
     String generatedStr = generator.generate();
     assertEquals(generatedStr.length(), 4);
   }
 
   @Test
-  @RepeatedTest(10)
   public void shouldGenerateNumberWithDigits() {
     String generatedStr = generator.generate();
     for (char c:
         generatedStr.toCharArray()) {
       assertEquals(true, Character.isDigit(c));
     }
+  }
+
+  @Test
+  @RepeatedTest(10)
+  public void shouldGenerateNumberWithDistinctDigits() {
+    String generatedStr = generator.generate();
+    HashSet<Character> chars  = new HashSet<Character>();
+    for (char c:
+        generatedStr.toCharArray()) {
+      chars.add(Character.valueOf(c));
+    }
+    assertEquals(chars.size(), generatedStr.length());
   }
 }
