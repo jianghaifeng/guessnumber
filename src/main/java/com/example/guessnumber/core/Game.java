805130751d9f05ee35d523274d0797b809f465ee
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-26 11:52:29 +0800

    [testcase] Add the Game class and test case.

---
+++ src/main/java/com/example/guessnumber/core/Game.java
@@ -0,0 +1,12 @@
+package com.example.guessnumber.core;
+
+public class Game {
+  private String targetNumberStr;
+
+  public Game() {
+  }
+
+  public String getTargetNumberStr() {
+    return targetNumberStr;
+  }
+}
---
+++ src/test/java/com/example/guessnumber/core/GameTest.java
@@ -0,0 +1,26 @@
+package com.example.guessnumber.core;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GameTest {
+
+  @Mock
+  RandomNumberGenerator generator;
+
+  @Test
+  public void shouldGenerateNumberWhenGameCeated() {
+    when(generator.generate()).thenReturn("1234");
+
+    Game game = new Game(generator);
+
+    assertEquals("1234", game.getTargetNumberStr());
+  }
+}
