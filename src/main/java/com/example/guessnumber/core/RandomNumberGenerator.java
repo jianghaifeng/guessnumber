ceddcaeae0808ddf31cef7b781c6ccc898184206
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-26 10:36:45 +0800

    [refactor] Pass test cases.

--- src/main/java/com/example/guessnumber/core/RandomNumberGenerator.java
+++ src/main/java/com/example/guessnumber/core/RandomNumberGenerator.java
@@ -1,12 +1,14 @@
 package com.example.guessnumber.core;
 
+import java.util.Random;
+
 public class RandomNumberGenerator {
   public String generate() {
+    Random random = new Random();
     String str = "";
     for (int i = 0; i < 4; i++) {
-      str += " ";
+      str += String.valueOf(random.nextInt(10));
     }
-
     return str;
   }
 }
