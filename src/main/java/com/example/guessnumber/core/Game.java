e6a00df27d0e69e235e1e61a66eb8fd721cf0971
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-26 14:53:05 +0800

    [refactor] Remove the class GameAnswerArbitrator, wrap a arbitrate method in GameAnswer.

--- src/main/java/com/example/guessnumber/core/Game.java
+++ src/main/java/com/example/guessnumber/core/Game.java
@@ -2,21 +2,13 @@ package com.example.guessnumber.core;
 
 public class Game {
   private GameAnswer target;
-  private GameAnswerArbitrator arbitrator;
 
   public Game() {
     RandomNumberGenerator generator = new RandomNumberGenerator();
-    this.arbitrator =  new GameAnswerArbitrator();
     this.target = new GameAnswer(generator.generate());
   }
 
   public Game(RandomNumberGenerator generator) {
-    arbitrator = new GameAnswerArbitrator();
-    this.target = new GameAnswer(generator.generate());
-  }
-
-  public Game(RandomNumberGenerator generator, GameAnswerArbitrator arbitrator) {
-    this.arbitrator = arbitrator;
     this.target = new GameAnswer(generator.generate());
   }
 
@@ -25,6 +17,6 @@ public class Game {
   }
 
   public String guess(GameAnswer candidate) {
-    return arbitrator.check(target, candidate);
+    return target.arbitrate(candidate);
   }
 }
--- src/main/java/com/example/guessnumber/core/GameAnswer.java
+++ src/main/java/com/example/guessnumber/core/GameAnswer.java
@@ -10,4 +10,19 @@ public class GameAnswer {
   public String getAnswer() {
     return answer;
   }
+
+  public String arbitrate(GameAnswer candidate) {
+    String candidateStr = candidate.getAnswer();
+
+    int aQualified = 0, bQualified = 0;
+
+    for (int i = 0; i < 4; i++) {
+      if (candidateStr.charAt(i) == answer.charAt(i)) {
+        aQualified++;
+      } else if (answer.indexOf(candidateStr.charAt(i)) != -1) {
+        bQualified++;
+      }
+    }
+    return String.format("%dA%dB", aQualified, bQualified);
+  }
 }
