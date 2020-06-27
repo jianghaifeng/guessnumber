c747c6cae6ac3abc2a9a33c12f09d479a1feca4b
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-28 00:51:39 +0800

    [refactor] build the game application.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import com.example.guessnumber.console.ConsoleGameApp;
+import com.example.guessnumber.console.ConsoleInput;
+import com.example.guessnumber.console.ConsoleOutput;
+import com.example.guessnumber.core.GameRecorder;
+import com.example.guessnumber.core.RandomAnswerGenerator;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello");
+    ConsoleGameApp game = new ConsoleGameApp(
+        new RandomAnswerGenerator(),
+        new GameRecorder(),
+        new ConsoleOutput(),
+        new ConsoleInput()
+    );
+
+    game.play();
   }
 }
