bfc6b21ca3cb5a7f08c7ae257ef1e37fefcc3d64
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-27 18:26:44 +0800

    [refactor] create a new class ConsoleInput to get input as a GameAnswer object.

---
+++ src/main/java/com/example/guessnumber/console/ConsoleInput.java
@@ -0,0 +1,9 @@
+package com.example.guessnumber.console;
+
+import com.example.guessnumber.core.GameAnswer;
+
+public class ConsoleInput {
+  public GameAnswer inputAnswer() {
+    return new GameAnswer("");
+  }
+}
