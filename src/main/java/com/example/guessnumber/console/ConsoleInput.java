1b1da48ce0d62f20123e542477441feeaeeffc7d
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-27 20:35:18 +0800

    [refactor] Pass test cases.

--- src/main/java/com/example/guessnumber/console/ConsoleInput.java
+++ src/main/java/com/example/guessnumber/console/ConsoleInput.java
@@ -2,8 +2,21 @@ package com.example.guessnumber.console;
 
 import com.example.guessnumber.core.GameAnswer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleInput {
   public GameAnswer inputAnswer() {
-    return new GameAnswer("");
+    InputStreamReader reader = new InputStreamReader(System.in);
+    BufferedReader bufferedReader = new BufferedReader(reader);
+
+    String input = null;
+    try {
+      input = bufferedReader.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new GameAnswer(input);
   }
 }
