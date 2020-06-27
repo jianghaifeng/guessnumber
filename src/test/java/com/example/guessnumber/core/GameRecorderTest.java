0451953f93aec520b9913ad9293e3a3517eb4f0a
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-27 12:24:58 +0800

    [testcase] should return the right string pattern for guess history.

--- src/test/java/com/example/guessnumber/core/GameRecorderTest.java
+++ src/test/java/com/example/guessnumber/core/GameRecorderTest.java
@@ -2,19 +2,20 @@ package com.example.guessnumber.core;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameRecorderTest {
 
   @Test
   public void shouldHaveARecordWhen1RecordIsAdded() {
     GameRecorder recorder = new GameRecorder();
-    GameAnswer answer  = new GameAnswer("1234");
-    GameResult result = new GameResult(1,1);
+    GameAnswer answer = new GameAnswer("1234");
+    GameResult result = new GameResult(1, 1);
 
     assertEquals(0, recorder.getRecords().size());
 
     recorder.addRecord(answer, result);
     assertEquals(1, recorder.getRecords().size());
+    assertEquals("1234: 1A1B", recorder.getRecords().get(0));
   }
 }
