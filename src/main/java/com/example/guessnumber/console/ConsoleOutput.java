a8dc7b238d3ec26a52638a17afc78e2142890c87
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-27 20:38:13 +0800

    [refactor] Pass test cases.

--- src/main/java/com/example/guessnumber/console/ConsoleOutput.java
+++ src/main/java/com/example/guessnumber/console/ConsoleOutput.java
@@ -1,8 +1,8 @@
 package com.example.guessnumber.console;
 
-import com.example.guessnumber.core.GameResult;
-
 public class ConsoleOutput {
+
   public void output(String outStr) {
+    System.out.println(outStr);
   }
 }
